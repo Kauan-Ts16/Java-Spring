aebf490f2a55d19517ec4d62f03b9721e444560e
Kauan-Ts16 <Kauanfranca16@gmail.com>
2024-07-22 16:48:35 -0300

    Definindo segurança das rotas.

--- ParkingControlAPI/src/main/java/com/api/ParkingControlAPI/components/config/security/WebSecurityConfig.java
+++ ParkingControlAPI/src/main/java/com/api/ParkingControlAPI/components/config/security/WebSecurityConfig.java
@@ -2,21 +2,33 @@ package com.api.ParkingControlAPI.components.config.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-@EnableMethodSecurity
+
 public class WebSecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests((authorize) -> authorize
+                        .requestMatchers(HttpMethod.POST, "/parking-control/user").permitAll()
+
+                        .requestMatchers(HttpMethod.GET, "/parking-control/user/all").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.DELETE, "/parking-control/user/{id}").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.PUT, "/parking-control/user/admin/{id}").hasRole("ADMIN")
+
+                        .requestMatchers(HttpMethod.PUT, "/parking-control/user").hasAnyRole("ADMIN", "USER")
+                        .requestMatchers(HttpMethod.GET, "/parking-control/user/{id}").hasAnyRole("ADMIN", "USER")
+
+
+
+
                         .anyRequest().authenticated()
                 )
                 .httpBasic(Customizer.withDefaults())
