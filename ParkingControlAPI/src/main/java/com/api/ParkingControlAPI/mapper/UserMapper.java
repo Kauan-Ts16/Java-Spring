815de2dfb8d3d40042626b288da2764c51fdc84c
Kauan-Ts16 <Kauanfranca16@gmail.com>
2024-07-22 16:50:29 -0300

    Implementação de método toMapper para update.

--- ParkingControlAPI/src/main/java/com/api/ParkingControlAPI/mapper/UserMapper.java
+++ ParkingControlAPI/src/main/java/com/api/ParkingControlAPI/mapper/UserMapper.java
@@ -1,6 +1,7 @@
 package com.api.ParkingControlAPI.mapper;
 
 import com.api.ParkingControlAPI.dto.post.UserPost;
+import com.api.ParkingControlAPI.dto.put.UserPut;
 import com.api.ParkingControlAPI.model.UserModel;
 import org.springframework.beans.BeanUtils;
 
@@ -8,6 +9,11 @@ public class UserMapper {
 
     public static UserModel toMapper(UserPost input) {
         UserModel output = new UserModel();
+        BeanUtils.copyProperties(input, output);
+        return output;
+    }
+
+    public static UserModel toMapper(UserPut input, UserModel output) {
         BeanUtils.copyProperties(input, output);
         return output;
     }
