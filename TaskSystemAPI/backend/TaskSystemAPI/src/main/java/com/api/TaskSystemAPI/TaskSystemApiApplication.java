6c078b7f1f7ebd829796f09fbb69b2351da5ef8a
Kauan-Ts16 <Kauanfranca16@gmail.com>
2024-10-22 18:21:06 -0300

    Upload do Projeto.

---
+++ TaskSystemAPI/backend/TaskSystemAPI/src/main/java/com/api/TaskSystemAPI/TaskSystemApiApplication.java
@@ -0,0 +1,13 @@
+package com.api.TaskSystemAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskSystemApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskSystemApiApplication.class, args);
+	}
+
+}
---
+++ TaskSystemAPI/backend/TaskSystemAPI/src/test/java/com/api/TaskSystemAPI/TaskSystemApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.TaskSystemAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskSystemApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
