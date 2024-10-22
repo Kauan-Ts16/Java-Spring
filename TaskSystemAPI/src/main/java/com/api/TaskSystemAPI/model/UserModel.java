b83086a95b0fd28ca89d10321da72c8d7f4eb48f
Kauan-Ts16 <Kauanfranca16@gmail.com>
2024-10-22 18:51:48 -0300

    Models 100% Finalizadas.

---
+++ TaskSystemAPI/src/main/java/com/api/TaskSystemAPI/model/UserModel.java
@@ -0,0 +1,43 @@
+package com.api.TaskSystemAPI.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.Set;
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "tb_users")
+public class UserModel implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "user_id")
+    private UUID userId;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "tb_user_role",
+        joinColumns = @JoinColumn(name = "user_id"),
+        inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<RoleModel> roles;
+
+}
